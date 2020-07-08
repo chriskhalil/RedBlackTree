025ee49edbcd3688d25ef1826873f52330e30068
chriskhalil <57494425+chriskhalil@users.noreply.github.com>
2020-07-08 08:43:23 +0300

    Adding Tnill

--- MultiKeyRedBlackTree.h
+++ MultiKeyRedBlackTree.h
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <stack>
+
 enum Color :bool { Black = 0, Red = 1 };
 
 template<typename Tk, typename Tv>
@@ -14,8 +16,20 @@ struct Node {
 	Node* right;
 	bool is_nil;
 	bool color;
+	Node(){}
+	Node(Tk key) { _myval.first = key; }
 	Node(Tk k, Tv v, Node* p = nullptr, Node* l = nullptr, Node* r = nullptr)
-		:_myval(k, v), parent(p), left(l), right(r), is_nil(0),color(Red) {}
+		:_myval(k, v), parent(p), left(l), right(r), is_nil(0), color(Red) {
+	}
+	Node(const Node& temp) {
+		this->color = temp.color;
+		this->left = temp.left;
+		this->right = temp.right;
+		this->parent = temp.parent;
+		this->_myval.first = temp._myval.first;
+		this->_myval.second = temp._myval.second;
+
+	}
 };
 
 template<typename Tk, typename Tv>
@@ -23,26 +37,53 @@ class RedBlackTree {
 private:
 	int _count;
 	Node<Tk, Tv>* _root;
+	Node<Tk, Tv>* _sentinelNill;
 	void binarySearchTreeInsert(Node<Tk, Tv>*, Node<Tk, Tv>*);
 	void leftRotate(Node<Tk, Tv>*, Node<Tk, Tv>*);
 	void rightRotate(Node<Tk, Tv>*, Node<Tk, Tv>*);
 	void fixRbtInsert(Node<Tk, Tv>*);
 	void fixRbtDelete(Node<Tk, Tv>*);
 	void DeleteTree(Node<Tk, Tv>*);
+	Node<Tk, Tv>* treeSuccessor(Node <Tk, Tv>*);
 
 
 public:
+	class iterator {
+		std::stack<Node<Tk, Tv>*> storage;
+	public:
+		iterator(Node<Tk, Tv>* node) {
+			while (node != nullptr) {
+				storage.push(node);
+				node = node->left;
+			}
+		}
+		Node<Tk, Tv>* current() { return storage.top(); }
+		//implement here the usual operators 
+		// for the iterator, i.e.: ++,--, ==,!=,*,->
+		/*void overload operator++() {
+			auto current = storage.top()->right;
+			storage.pop();
+			while (current != nullptr) {
+				storage.push;
+				current = current->left;
+			}*/
+
+	};
 	RedBlackTree();
-	void insert(std::pair<Tk,Tv>);
+	void insert(std::pair<Tk, Tv>);
+	void insert(Node<Tk, Tv>*);
 	void remove(Tk);
-	const Node<Tk, Tv>* Find(Tk);
+	Node<Tk, Tv>* Find(Tk);
 	std::string print();
-	const Node<Tk, Tv>* inOrderTraversal(const Node<Tk, Tv>*);
+	const Node<Tk, Tv>* inOrderTraversal(const Node<Tk, Tv>*, std::ostream& os);
 	const Node<Tk, Tv>* GetRoot() { return _root; }
+	Node<Tk, Tv>* findMin( Node<Tk, Tv>*);
+	Node<Tk, Tv>* findMax( Node<Tk, Tv>*);
 
 
 	~RedBlackTree() {
 		DeleteTree(_root);
+		delete _sentinelNill;
 	}
 };
 #pragma region CtorDtor
@@ -51,6 +92,8 @@ template<typename Tk, typename Tv>
 RedBlackTree<Tk, Tv>::RedBlackTree() {
 	_count = 0;
 	_root = nullptr;
+	_sentinelNill = new Node<Tk, Tv>();
+	_sentinelNill->is_nil = true;
 }
 template<typename Tk, typename Tv>
 void RedBlackTree<Tk, Tv>::DeleteTree(Node<Tk, Tv>* t) {
@@ -75,16 +118,16 @@ inline void RedBlackTree<Tk, Tv>::binarySearchTreeInsert(Node<Tk, Tv>* node, Nod
 		return;
 	}
 
-	if (element->_myval.first < node->_myval.first && node->left != nullptr)
+	if (element->_myval.first < node->_myval.first && !node->left->is_nil)
 		binarySearchTreeInsert(node->left, element);
-	else if (element->_myval.first < node->_myval.first && node->left == nullptr)
+	else if (element->_myval.first < node->_myval.first && node->left->is_nil)
 	{
 		node->left = element;
 		element->parent = node;
 	}
-	else if (element->_myval.first > node->_myval.first&& node->right != nullptr)
+	else if (element->_myval.first > node->_myval.first && !node->right->is_nil)
 		binarySearchTreeInsert(node->right, element);
-	else if (element->_myval.first > node->_myval.first&& node->right == nullptr)
+	else if (element->_myval.first > node->_myval.first && node->right->is_nil)
 	{
 		node->right = element;
 		element->parent = node;
@@ -191,16 +234,73 @@ inline void RedBlackTree<Tk, Tv>::fixRbtInsert(Node<Tk, Tv>* node)
 	_root->color = Black;
 }
 template<typename Tk, typename Tv>
-inline void RedBlackTree<Tk, Tv>::fixRbtDelete(Node<Tk, Tv>*)
+inline void RedBlackTree<Tk, Tv>::fixRbtDelete(Node<Tk, Tv>* x)
 {
-	//TODO
+	while (x != _root && x->color == Black)
+	{
+		auto sib = x->parent->right;
+		if (x == x->parent->left) {
+			//case 1
+			if (sib->color == Red) {
+				sib->color = Black;
+				x->parent->color = Red;
+				leftRotate(_root, x->parent);
+				sib = x->parent->right;
+			}
+			if (sib->left->color == Black && sib->right->color == Black) {
+				sib->color = Red;
+				x = x->parent;
+			}
+			else if (sib->right->color == Black) {
+				sib->left->color = Black;
+				sib->color = Red;
+				rightRotate(_root, sib);
+				sib = x->parent->right;
+			}
+			else {
+				sib->color = x->parent->color;
+				x->parent->color = Black;
+				sib->right->color = Black;
+				leftRotate(_root, x->parent);
+				x = _root;
+			}
+		}
+		else {
+			if (sib->color == Red) {
+				sib->color = Black;
+				x->parent->color = Red;
+				rightRotate(_root, x->parent);
+				sib = x->parent->left;
+			}
+			if (sib->right->color == Black && sib->left->color == Black) {
+				sib->color = Red;
+				x = x->parent;
+			}
+			else if (sib->left->color == Black) {
+				sib->right->color = Black;
+				sib->color = Red;
+				leftRotate(_root, sib);
+				sib = x->parent->left;
+			}
+			else {
+				sib->color = x->parent->color;
+				x->parent->color = Black;
+				sib->left->color = Black;
+				rightRotate(_root, x->parent);
+				x = _root;
+			}
+		}
+	}
 }
 #pragma endregion
 
 #pragma region Operations
 template<typename Tk, typename Tv>
-inline void RedBlackTree<Tk, Tv>::insert(const std::pair<Tk,Tv> element) {
-	Node<Tk, Tv>* temporal_node = new Node<Tk, Tv>(element.first,element.second);
+inline void RedBlackTree<Tk, Tv>::insert(const std::pair<Tk, Tv> element) {
+	Node<Tk, Tv>* temporal_node = new Node<Tk, Tv>(element.first, element.second);
+	//point to _sentinal
+	temporal_node->right = temporal_node->left = _sentinelNill;
+
 	binarySearchTreeInsert(_root, temporal_node);
 	_count++;
 	//check red black tree for violation
@@ -208,9 +308,17 @@ inline void RedBlackTree<Tk, Tv>::insert(const std::pair<Tk,Tv> element) {
 	if (_count > 1)
 		fixRbtInsert(temporal_node);
 }
-
 template<typename Tk, typename Tv>
-inline const Node<Tk, Tv>* RedBlackTree<Tk, Tv>::Find(Tk key) {
+inline void RedBlackTree<Tk, Tv>::insert(Node<Tk, Tv>* node) {
+	binarySearchTreeInsert(_root, node);
+	_count++;
+	//check red black tree for violation
+	//do not fix if we insert root
+	if (_count > 1)
+		fixRbtInsert(node);
+}
+template<typename Tk, typename Tv>
+inline Node<Tk, Tv>* RedBlackTree<Tk, Tv>::Find(Tk key) {
 	auto node = _root;
 	if (_root == nullptr) return nullptr;
 	while (node != nullptr) {
@@ -227,20 +335,59 @@ inline const Node<Tk, Tv>* RedBlackTree<Tk, Tv>::Find(Tk key) {
 			return node;
 		}
 	}
-	std::cout << "The tree does not contain node with key:" << node->_myval.first << "\n";
+	std::cout << "The tree does not contain node with key:" << key << "\n";
 	return nullptr;
 }
-
+template<typename Tk, typename Tv>
+inline Node<Tk, Tv>* RedBlackTree<Tk, Tv>::findMin(Node<Tk, Tv>* node) {
+	auto temp = node;
+	while (temp->left != nullptr)
+		temp = temp->left;
+	return temp;
+}
+template<typename Tk, typename Tv>
+inline Node<Tk, Tv>* RedBlackTree<Tk, Tv>::findMax(Node<Tk, Tv>* node) {
+	auto temp = node;
+	while (temp->right != nullptr)
+		temp = temp->right;
+	return temp;
+}
 
 template<typename Tk, typename Tv>
 inline void RedBlackTree<Tk, Tv>::remove(Tk key)
 {
 	if (_root == nullptr)return;
-	auto temp = Find(key);
+	Node<Tk,Tv>* temp = Find(key);
 	if (temp == nullptr)return;
 	else {
+		Node<Tk, Tv>* x;
+		Node<Tk, Tv>* y;
+		if (temp->left == nullptr || temp->right == nullptr)
+			y = temp;
+		else
+			//get the inorder successor of the current node
+			y = treeSuccessor(temp);
+		if (y->left != nullptr)
+			x = y->left;
+		else
+			x = y->right;
+		x->parent = y->parent;
+		if (y->parent == nullptr)
+			_root = x;
+		else if (y == y->parent->left)
+			y->parent->left = x;
+		else
+			y->parent->right = x;
 
+		if (y != temp) {
+			temp = nullptr;
+			temp=y;
+		}
+		if (y->color == Black)
+			fixRbtDelete(x);
 		_count--;
+		//return y;
+		delete y;
 	}
 
 }
@@ -248,6 +395,26 @@ inline void RedBlackTree<Tk, Tv>::remove(Tk key)
 
 
 #pragma region Traversal
+
+template<typename Tk, typename Tv>
+Node<Tk, Tv>* RedBlackTree<Tk, Tv>::treeSuccessor(Node<Tk, Tv>* node) {
+
+	if (_root == nullptr)
+		return nullptr;
+
+	if (node->right != nullptr)
+		return findMin(node->right);
+
+	auto temp = node->parent;
+
+	while (temp != nullptr && node == temp->right) {
+		node = temp;
+		temp = temp->parent;
+	}
+	return temp;
+}
+
+
 template<typename Tk, typename Tv>
 std::string RedBlackTree<Tk, Tv>::print() {
 
@@ -283,21 +450,20 @@ std::string RedBlackTree<Tk, Tv>::print() {
 	return ss.str();
 }
 
-
-
-
-
 template<typename Tk, typename Tv>
-const Node<Tk, Tv>* RedBlackTree<Tk, Tv>::inOrderTraversal(const Node<Tk, Tv>* node)
+const Node<Tk, Tv>* RedBlackTree<Tk, Tv>::inOrderTraversal(const Node<Tk, Tv>* node, std::ostream& os)
 {
-	if (node == NULL)
+	if (node == nullptr || node->is_nil)
 		return nullptr;
 
-	inOrderTraversal(node->left);
-	std::cout << node->_myval.first << "  ";
-	inOrderTraversal(node->right);
+	inOrderTraversal(node->left, os);
+	os << node->_myval.first << "=>";
+	inOrderTraversal(node->right, os);
 }
 
 #pragma endregion
 
+#pragma region Iterator
+
+#pragma endregion
 
--- Source.cpp
+++ Source.cpp
@@ -4,27 +4,69 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#define  p std::pair<std::string,int>
+#define  p std::pair<int,int>
 
 int main() {
-	RedBlackTree<std::string,int> dt;
-
+	RedBlackTree<int,int> dt;
+	std::stringstream ss;
 	//std::vector<int> v = {8,18,5,15,17,25,40,80}; //pass inorderT std::vector<int> as = {5,8,15,17,18,25,40,80};
 	//std::vector<int> v = {38,13,51,10,12,40,84,25}; //pass inorder 10  12  13  25  38  40  51  84
 	//std::vector<std::pair<int,int>> v =std::vector<std::pair<int,int>> v = {p(38,1),p(13,1),p(51,1),p(10,1),p(12,1),p(40,1),p(84,1),p(25,1)}; //pass inorder 10  12  13  25  38  40  51  84
-	std::vector<std::pair<std::string,int>> v = {p("38",1),p("13",1),p("51",1),p("10",1),p("12",1),p("40",1),p("84",1),p("25",1)}; //pass inorder 10  12  13  25  38  40  51  84
+	//std::vector<std::pair<std::string,int>> v = {p("38",1),p("13",1),p("51",1),p("10",1),p("12",1),p("40",1),p("84",1),p("25",1)}; //pass inorder 10  12  13  25  38  40  51  84
+	
+	std::vector<std::pair<int,int>> v = {p(11,1),p(15,1),p(14,1),p(1,1),p(7,1),p(5,1),p(8,1)};
 
 	for (auto& x : v)
 		dt.insert(x);
 
-	
-	/*std::cout << "---------------------------------------------\n";
-	dt.Find(5);
-	std::cout << "---------------------------------------------\n";
-	dt.Find(400);
-	std::cout << "---------------------------------------------\n";*/
+	ss.str(std::string());
+	std::cout << "Without key 2\n";
 	std::cout << "InOrder Traversal: ";
-	dt.inOrderTraversal(dt.GetRoot());
+	dt.inOrderTraversal(dt.GetRoot(), ss);
+	std::cout << ss.str();
+
+	std::cout << "\n---------------------------------------------\n";
+	
+	ss.str(std::string());
+	std::cout << "Insert key 2\n";
+	dt.insert(p(2, 1));
+	std::cout << "InOrder Traversal: ";
+	dt.inOrderTraversal(dt.GetRoot(), ss);
+	std::cout << ss.str();
+
+	std::cout << "\n---------------------------------------------\n";
+
+	ss.str(std::string());
+
+	std::cout << "InOrder Traversal: ";
+	dt.inOrderTraversal(dt.GetRoot(),ss);
+	std::cout << ss.str();
+	std::cout << "\n---------------------------------------------\n";
+
+	ss.str(std::string());
+	std::cout << "Insert key 4\n";
+	dt.insert(p(4, 1));
+	std::cout << "InOrder Traversal: ";
+	dt.inOrderTraversal(dt.GetRoot(), ss);
+	std::cout << ss.str();
+
+	std::cout << "\n---------------------------------------------\n";
+	ss.str(std::string());
+	std::cout << "Deleted key 4\n";
+	dt.remove(4);
+	std::cout << "InOrder Traversal: ";
+	dt.inOrderTraversal(dt.GetRoot(), ss);
+	std::cout << ss.str();
+	std::cout << "\n---------------------------------------------\n";
+
+
+	std::cout << "\n---------------------------------------------\n";
+	ss.str(std::string());
+	std::cout << "Deleted key 2\n";
+	dt.remove(2);
+	std::cout << "InOrder Traversal: ";
+	dt.inOrderTraversal(dt.GetRoot(), ss);
+	std::cout << ss.str();
 	std::cout << "\n---------------------------------------------\n";
 	return 0;
 }
